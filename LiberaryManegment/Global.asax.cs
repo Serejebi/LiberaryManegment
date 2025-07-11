8d147f585348622d9308bdfba37294cdc10bbd21
Toranj Digital <Toranj Digital@DESKTOP-EFTQVNL>
2025-07-11 15:23:15 +0330

    Revert "استفاده از RESTfull"
    This reverts commit d78b7fd157b7724a281ed186ae07506cdb5ae33f.

--- LiberaryManegment/Global.asax.cs
+++ LiberaryManegment/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,11 +13,9 @@ namespace LiberaryManegment
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);  // Web API
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);         // MVC
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
     }
 }
